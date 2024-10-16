26e6920e01c72736ef32b2f5ef6456ff6b17f267
ashwaniSahu273 <ashwanisahu@resiliencesoft.com>
2024-10-16 18:52:13 +0530

    add some new fetures

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/Providers/movie_model.dart';
+import 'package:movie_app/Widgets/movie_details.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -39,22 +40,75 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Movies"),
+        title: Text(
+          "Movies",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
       ),
       body: Center(
         child: ListView.builder(
           itemCount: movieProvider.movieList.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(movieProvider.movieList[index]),
-              subtitle: Text("This is a subtitle"),
-              trailing: Icon(Icons.sunny),
-              isThreeLine: false,
-              leading: CircleAvatar(
-                child: Text(movieProvider.movieList[index].substring(0, 1)),
+            return Card(
+              margin: EdgeInsets.all(10),
+              child: ExpansionTile(
+                title: Text(movieProvider.movieList[index]),
+                subtitle: Text("Director : mohan kumar"),
+                leading: CircleAvatar(
+                  child: Text(movieProvider.movieList[index].substring(0, 1)),
+                ),
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.only(left: 75),
+                    child: Column(
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                            text: TextSpan(children: [
+                          TextSpan(
+                              text: "Released : ",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold)),
+                          const TextSpan(
+                              text: "24th oct 2004\n",
+                              style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                              text: "Plot: ",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold)),
+                          const TextSpan(
+                              text:
+                                  "This is a text this is a text text text textThis is a text this is a text text text textThis is a text this is a text text text textThis is a text this is a text text text text",
+                              style: TextStyle(color: Colors.black))
+                        ]
+                        )
+                        ),
+                        TextButton(onPressed: (){
+                          Navigator.push(context,MaterialPageRoute(builder: (context)=>MovieDetails()));
+                        }, child: Text("Read More", style: TextStyle(color: Colors.blue),))
+                      ],
+                    ),
+                  )
+                ],
               ),
-            
             );
+
+            // ListTile(
+            //   title: Text(movieProvider.movieList[index]),
+            //   subtitle: Text("This is a subtitle"),
+            //   trailing: Icon(Icons.sunny),
+            //   isThreeLine: false,
+            //   leading: CircleAvatar(
+            //     child: Text(movieProvider.movieList[index].substring(0, 1)),
+            //   ),
+
+            // );
           },
         ),
       ),
